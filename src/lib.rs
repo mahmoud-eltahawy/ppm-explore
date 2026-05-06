0947aca20302faf3ce305c0da1dfa3dbb4c6dde3
mahmoud-eltahawy <gemmy9300@gmail.com>
2026-05-06 10:35:59 +0300

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,27 @@
-use std::ops::{Add, Div, Mul, Sub};
+use std::{
+    array::from_fn,
+    mem::{ManuallyDrop, MaybeUninit},
+    ops::{Add, Div, Mul, Sub},
+};
 
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Vector<const L: usize, V>(pub [V; L]);
+
+impl<const L: usize, V: Default + Copy> Default for Vector<L, V> {
+    fn default() -> Self {
+        Self::splat(V::default())
+    }
+}
 
 impl<const L: usize, V> Vector<L, V>
 where
-    V: Mul<Output = V> + Clone + Copy + Default + Into<f32>,
+    V: Copy,
 {
     pub const fn splat(v: V) -> Self {
         Self([v; L])
     }
-    pub fn nth(&self, i: usize) -> V {
-        self.0[i]
+    pub fn nth(&self, i: usize) -> Option<V> {
+        if i < L { Some(self.0[i]) } else { None }
     }
     pub fn x(&self) -> V {
         self.0[0]
@@ -18,32 +29,51 @@ where
     pub fn y(&self) -> V {
         self.0[1]
     }
-    pub fn z(&self) -> V {
-        assert_eq!(L, 3, "cannot access z in vec2 or less");
-        self.0[2]
-    }
-    pub fn w(&self) -> V {
-        assert_eq!(L, 4, "cannot access z in vec3 or less");
-        self.0[3]
-    }
+}
+
+impl<const L: usize, V: Mul<Output = V> + Copy + Into<f32>> Vector<L, V> {
     pub fn length(&self) -> f32 {
         self.0
+            .iter()
             .map(|x| {
-                let x: f32 = (x * x).into();
+                let x: f32 = (*x * *x).into();
                 x
             })
-            .iter()
             .sum::<f32>()
             .sqrt()
     }
 }
 
+impl<V> Vector<3, V>
+where
+    V: Copy,
+{
+    pub fn z(&self) -> V {
+        self.0[2]
+    }
+}
+
+impl<V> Vector<4, V>
+where
+    V: Copy,
+{
+    pub fn z(&self) -> V {
+        self.0[2]
+    }
+    pub fn w(&self) -> V {
+        self.0[3]
+    }
+}
+
 pub type Vec2<V> = Vector<2, V>;
 pub type Vec3<V> = Vector<3, V>;
+pub type Vec4<V> = Vector<4, V>;
 pub type Vec2f32 = Vec2<f32>;
 pub type Vec3f32 = Vec3<f32>;
+pub type Vec4f32 = Vec4<f32>;
 pub type Vec2f64 = Vec2<f64>;
 pub type Vec3f64 = Vec3<f64>;
+pub type Vec4f64 = Vec4<f64>;
 
 impl<V> Vec2<V> {
     pub const fn new(x: V, y: V) -> Self {
@@ -57,54 +87,75 @@ impl<V> Vec3<V> {
     }
 }
 
-impl<const L: usize, V: Add<Output = V> + Clone + Copy + Default> Add for Vector<L, V> {
+impl<V> Vec4<V> {
+    pub const fn new(x: V, y: V, z: V, w: V) -> Self {
+        Self([x, y, z, w])
+    }
+}
+
+union ArrayTransmute<T, const N: usize> {
+    from: ManuallyDrop<[MaybeUninit<T>; N]>,
+    to: ManuallyDrop<[T; N]>,
+}
+
+impl<const L: usize, V: Add<Output = V> + Copy> Add for Vector<L, V> {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
-        assert_eq!(self.0.len(), rhs.0.len(), "vectors must be the same size");
-        let mut res: [V; L] = [V::default(); L];
+        let mut res: [MaybeUninit<V>; L] = from_fn(|_| MaybeUninit::uninit());
+
         for (i, (a, b)) in self.0.iter().zip(rhs.0).enumerate() {
-            res[i] = *a + b;
+            res[i].write(*a + b);
         }
-        Self(res)
+
+        let union = ArrayTransmute {
+            from: ManuallyDrop::new(res),
+        };
+        Self(unsafe { ManuallyDrop::into_inner(union.to) })
     }
 }
 
-impl<const L: usize, V: Sub<Output = V> + Clone + Copy + Default> Sub for Vector<L, V> {
+impl<const L: usize, V: Sub<Output = V> + Copy> Sub for Vector<L, V> {
     type Output = Self;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        assert_eq!(self.0.len(), rhs.0.len(), "vectors must be the same size");
-        let mut res = [V::default(); L];
+        let mut res: [MaybeUninit<V>; L] = from_fn(|_| MaybeUninit::uninit());
         for (i, (a, b)) in self.0.iter().zip(rhs.0).enumerate() {
-            res[i] = *a - b;
+            res[i].write(*a - b);
         }
-        Self(res)
+        let union = ArrayTransmute {
+            from: ManuallyDrop::new(res),
+        };
+        Self(unsafe { ManuallyDrop::into_inner(union.to) })
     }
 }
 
-impl<const L: usize, V: Div<Output = V> + Clone + Copy + Default> Div for Vector<L, V> {
+impl<const L: usize, V: Div<Output = V> + Copy> Div for Vector<L, V> {
     type Output = Self;
 
     fn div(self, rhs: Self) -> Self::Output {
-        assert_eq!(self.0.len(), rhs.0.len(), "vectors must be the same size");
-        let mut res = [V::default(); L];
+        let mut res: [MaybeUninit<V>; L] = from_fn(|_| MaybeUninit::uninit());
         for (i, (a, b)) in self.0.iter().zip(rhs.0).enumerate() {
-            res[i] = *a / b;
+            res[i].write(*a / b);
         }
-        Self(res)
+        let union = ArrayTransmute {
+            from: ManuallyDrop::new(res),
+        };
+        Self(unsafe { ManuallyDrop::into_inner(union.to) })
     }
 }
 
-impl<const L: usize, V: Mul<Output = V> + Clone + Copy + Default> Mul for Vector<L, V> {
+impl<const L: usize, V: Mul<Output = V> + Copy> Mul for Vector<L, V> {
     type Output = Self;
 
     fn mul(self, rhs: Self) -> Self::Output {
-        assert_eq!(self.0.len(), rhs.0.len(), "vectors must be the same size");
-        let mut res = [V::default(); L];
+        let mut res: [MaybeUninit<V>; L] = from_fn(|_| MaybeUninit::uninit());
         for (i, (a, b)) in self.0.iter().zip(rhs.0).enumerate() {
-            res[i] = *a * b;
+            res[i].write(*a * b);
         }
-        Self(res)
+        let union = ArrayTransmute {
+            from: ManuallyDrop::new(res),
+        };
+        Self(unsafe { ManuallyDrop::into_inner(union.to) })
     }
 }
